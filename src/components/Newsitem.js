abdd006149daa5cdce1f852c2eb93837d675acf9
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-05-16 14:00:06 +0530

    news items

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
+
+    let {title,description} = this.props;
+
     return (
       <div>
-        this is newsitem
+        <div className="card" style={{width: '18rem'}}>
+          <img className="card-img-top" src="https://cdn.vox-cdn.com/thumbor/NMjsanOXQMWIIZJcE7Sm7wrSNu8=/0x75:1920x1080/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/24906992/chants_of_sennaar_puzzles.jpg" alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
         
       </div>
     )
--- src/components/news.js
+++ src/components/news.js
@@ -4,9 +4,29 @@ import Newsitem from './Newsitem';
 export class news extends Component {
   render() {
     return (
-      <div>
-        this  is news component
-        <Newsitem/>
+      <div className="container my-3">
+        <h2>Madsnews - top headlines</h2>
+
+
+        <div className="row">
+
+            <div className="col-md-3">
+                <Newsitem title="myTitle" description=' my des' />
+            </div>
+
+            <div className="col-md-3">
+                <Newsitem title="myTitle" description=' my des' />
+            </div>
+
+            <div className="col-md-3">
+                <Newsitem title="myTitle" description=' my des' />
+            </div>
+
+           
+
+        </div>
+        
+        
       </div>
     )
   }
