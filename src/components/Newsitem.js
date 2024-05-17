5680f9eed93cc2fe789a8361bd92b0c64968486f
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-05-17 20:03:24 +0530

    url

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 
 export class Newsitem extends Component {
+
+  
+
   render() {
 
-    let {title,description} = this.props;
+    let {title,description, imageUrl,newsUrl} = this.props;
 
     return (
-      <div>
+      <div className='my-5'>
         <div className="card" style={{width: '18rem'}}>
-          <img className="card-img-top" src="https://cdn.vox-cdn.com/thumbor/NMjsanOXQMWIIZJcE7Sm7wrSNu8=/0x75:1920x1080/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/24906992/chants_of_sennaar_puzzles.jpg" alt="Card image cap"/>
+          <img className="card-img-top" src={imageUrl} alt="Card image cap"/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">Read more</a>
           </div>
         </div>
         
